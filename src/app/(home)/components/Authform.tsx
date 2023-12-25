3823af2afe600f6007565ad32e7c16a898880cb0
SAHA-YJB <pkomom123@naver.com>
2023-12-25 20:02:07 +0900

    feaures: input/btn/socialbtn authform ui create

--- src/app/(home)/components/Authform.tsx
+++ src/app/(home)/components/Authform.tsx
@@ -1,9 +1,15 @@
+'use client';
+import Button from '@/components/Button';
+import Input from '@/components/inputs/Input';
 import axios from 'axios';
 import { signIn, useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import { toast } from 'react-hot-toast';
+import { BsGoogle } from 'react-icons/bs';
+import { RiKakaoTalkFill } from 'react-icons/ri';
+import AuthSocialButton from './AuthSocialButton';
 
 type Varinat = 'LOGIN' | 'REGISTER';
 
@@ -73,7 +79,79 @@ const Authform = () => {
       .finally(() => setIsLoading(false));
   };
 
-  return <div>Authform</div>;
+  return (
+    <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
+      <div className='px-4 py-8 bg-white shadow sm:rounded-lg sm:px-10'>
+        <form className='space-y-6' onSubmit={handleSubmit(onSubmit)}>
+          {variant === 'REGISTER' && (
+            <Input
+              disabled={isLoading}
+              register={register}
+              errors={errors}
+              required
+              id='name'
+              label='이름'
+            />
+          )}
+          <Input
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+            id='email'
+            label='이메일'
+            type='email'
+          />
+          <Input
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+            id='password'
+            label='비밀번호'
+            type='password'
+          />
+          <div>
+            <Button disabled={isLoading} fullWidth type='submit'>
+              {variant === 'LOGIN' ? '로그인' : '회원가입'}
+            </Button>
+          </div>
+        </form>
+
+        <div className='mt-6'>
+          <div className='relative'>
+            <div className='absolute inset-0 flex items-center '>
+              <div className='w-full border-t border-gray-300' />
+            </div>
+            <div className='relative flex justify-center text-sm'>
+              <span className='px-2 text-gray-500 bg-white'>소셜 로그인</span>
+            </div>
+          </div>
+
+          <div className='flex gap-2 mt-6'>
+            <AuthSocialButton
+              icon={RiKakaoTalkFill}
+              onClick={() => socialAction('kakao')}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction('google')}
+            />
+          </div>
+        </div>
+        <div className='flex justify-center gap-2 px-2 mt-6 text-sm text-gray-500 '>
+          <div>
+            {variant === 'LOGIN'
+              ? '메신저를 처음 사용하시나요?'
+              : '이미 계정이 있으신가요?'}
+          </div>
+          <div onClick={toggleVariant} className='underline cursor-pointer'>
+            {variant === 'LOGIN' ? '계정 만들기' : '로그인하기'}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Authform;
