0297dba8bc5a50ac16b6a45994afb24785aedcfb
SAHA-YJB <pkomom123@naver.com>
2023-12-26 14:15:45 +0900

    features: desktopSideBar ui create

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,13 +1,13 @@
+import getCurrentUser from '@/app/actions/getCurrentUser';
 import React from 'react';
 import DesktopSidebar from './DesktopSidebar';
 import MobileFooter from './MobileFooter';
-import getCurrentUser from '@/app/actions/getCurrentUser';
 
 const Sidebar = async ({ children }: { children: React.ReactNode }) => {
   const currentUser = await getCurrentUser();
   return (
     <div className='h-full'>
-      <DesktopSidebar currentUser={currentUser}/>
+      <DesktopSidebar currentUser={currentUser} />
       <MobileFooter />
       <main className='h-full lg:pl-20'>{children}</main>
     </div>
